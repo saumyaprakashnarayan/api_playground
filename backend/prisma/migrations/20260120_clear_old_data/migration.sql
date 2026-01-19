d88593d0b7a1af41287908297671c957a26c580f
Saumya Narayan <saumyaprakashnarayan@gmail.com>
2026-01-20 00:47:36 +0530

    Add migration to clear old data and auto-seed with correct profile info

---
+++ backend/prisma/migrations/20260120_clear_old_data/migration.sql
@@ -0,0 +1,8 @@
+-- Clear old data to force reseed with new profile information
+DELETE FROM "ProjectSkill";
+DELETE FROM "ProjectLink";
+DELETE FROM "Project";
+DELETE FROM "Skill";
+DELETE FROM "Education";
+DELETE FROM "Link";
+DELETE FROM "Profile";
